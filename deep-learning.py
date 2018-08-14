7a9ebed2c4679d53250e4f16fb6c0d0d90941adc
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-08-13 21:23:53 -0300

    Building Neural Network Training data

--- deep-learning.py
+++ deep-learning.py
@@ -1,5 +1,5 @@
 import sc2
-from sc2 import run_game, maps, Race, Difficulty, position
+from sc2 import run_game, maps, Race, Difficulty, position, Result
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, \
  GATEWAY, CYBERNETICSCORE, STARGATE, VOIDRAY, \
@@ -7,6 +7,9 @@ from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, \
 import random
 import cv2
 import numpy as np
+import time
+
+HEADLESS = True
 
 
 # Protoss bot class
@@ -14,6 +17,15 @@ class ProtossBot(sc2.BotAI):
     def __init__(self):
         self.ITERATIONS_PER_MINUTE = 165
         self.MAX_WORKERS = 65
+        self.do_something_after = 0
+        self.train_data = []
+
+    def on_end(self, game_result):
+        print('--- on_end called ---')
+        print(game_result)
+
+        if game_result == Result.Victory:
+            np.save("train_data/{}.npy".format(str(int(time.time()))), np.array(self.train_data))
 
     # Execute at every step
     async def on_step(self, iteration):
@@ -50,7 +62,7 @@ class ProtossBot(sc2.BotAI):
         if y > self.game_info.map_size[1]:
             y = self.game_info.map_size[1]
 
-        go_to = position.Point2(position.Pointlike((x,y)))
+        go_to = position.Point2(position.Pointlike((x, y)))
         return go_to
 
     async def scout(self):
@@ -113,12 +125,39 @@ class ProtossBot(sc2.BotAI):
             pos = observer.position
             cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (255, 255, 255), -1)
 
+        # Draw minerals and vespene
+        line_max = 50
+        mineral_ratio = self.minerals / 1500
+        if mineral_ratio > 1.0:
+            mineral_ratio = 1.0
+
+        vespene_ratio = self.vespene / 1500
+        if vespene_ratio > 1.0:
+            vespene_ratio = 1.0
+
+        population_ratio = self.supply_left / self.supply_cap
+        if population_ratio > 1.0:
+            population_ratio = 1.0
+
+        plausible_supply = self.supply_cap / 200.0
+
+        military_weight = len(self.units(VOIDRAY)) / (self.supply_cap-self.supply_left)
+        if military_weight > 1.0:
+            military_weight = 1.0
+
+
+        cv2.line(game_data, (0, 19), (int(line_max*military_weight), 19), (250, 250, 200), 3) # worker/supply ratio
+        cv2.line(game_data, (0, 15), (int(line_max*plausible_supply), 15), (220, 200, 200), 3) # plausible supply (supply/200.0)
+        cv2.line(game_data, (0, 11), (int(line_max*population_ratio), 11), (150, 150, 150), 3) # population ratio (supply_left/supply)
+        cv2.line(game_data, (0, 7), (int(line_max*vespene_ratio), 7), (210, 200, 0), 3) # gas / 1500
+        cv2.line(game_data, (0, 3), (int(line_max*mineral_ratio), 3), (0, 255, 25), 3) # minerals minerals/1500
+
         # Flip horizontally to make our final fix in visual representation
-        flipped = cv2.flip(game_data, 0)
-        resize = cv2.resize(flipped, dsize=None, fx=2, fy=2)
-        
-        cv2.imshow('Intel', resize)
-        cv2.waitKey(1)
+        self.flipped = cv2.flip(game_data, 0)
+        if not HEADLESS:
+            resize = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
+            cv2.imshow('Intel', resize)
+            cv2.waitKey(1)
 
     # Build workers (PROBE sc2.constants)
     async def build_workers(self):
@@ -216,26 +255,39 @@ class ProtossBot(sc2.BotAI):
 
     # Attack the enemy
     async def attack(self):
-        # {UNIT: [n to fight, n to defend]}
-        aggressive_units = {
-            VOIDRAY: [8, 3]
-        }
+        if len(self.units(VOIDRAY).idle) > 0:
+            choice = random.randrange(0, 4)
+            target = False
+            if self.iteration > self.do_something_after:
+                if choice == 0:
+                    # no attack
+                    wait = random.randrange(20, 165)
+                    self.do_something_after = self.iteration + wait
 
-        for UNIT in aggressive_units:
-            # Attack the base
-            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
-                for unit in self.units(UNIT).idle:
-                    await self.do(unit.attack(self.find_target(self.state)))
+                elif choice == 1:
+                    #attack_unit_closest_nexus
+                    if len(self.known_enemy_units) > 0:
+                        target = self.known_enemy_units.closest_to(random.choice(self.units(NEXUS)))
 
-            # Attack the enemy army or common unit
-            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
-                if len(self.known_enemy_units) > 0:
-                    for unit in self.units(UNIT).idle:
-                        await self.do(unit.attack(random.choice(self.known_enemy_units)))
+                elif choice == 2:
+                    #attack enemy structures
+                    if len(self.known_enemy_structures) > 0:
+                        target = random.choice(self.known_enemy_structures)
+
+                elif choice == 3:
+                    #attack_enemy_start
+                    target = self.enemy_start_locations[0]
+
+                if target:
+                    for vr in self.units(VOIDRAY).idle:
+                        await self.do(vr.attack(target))
+                y = np.zeros(4)
+                y[choice] = 1
+                self.train_data.append([y, self.flipped])
 
 
 # Run the game
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, ProtossBot()),
-    Computer(Race.Terran, Difficulty.Hard)
+    Computer(Race.Terran, Difficulty.Easy)
 ], realtime=False)
