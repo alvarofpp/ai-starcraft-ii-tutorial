bd0da25d99e74a6e713e5914e105559ac829bbef
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-09-17 00:42:29 -0300

    More Choices for AI

--- deep-learning.py
+++ deep-learning.py
@@ -3,7 +3,7 @@ from sc2 import run_game, maps, Race, Difficulty, position, Result
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, \
     GATEWAY, CYBERNETICSCORE, STARGATE, VOIDRAY, \
-    OBSERVER, ROBOTICSFACILITY
+    OBSERVER, ROBOTICSFACILITY, ZEALOT, STALKER
 import random
 import cv2
 import numpy as np
@@ -16,54 +16,61 @@ HEADLESS = True
 
 # Protoss bot class
 class ProtossBot(sc2.BotAI):
-    def __init__(self, use_model=False):
+    def __init__(self, use_model=False, title=1):
         # self.ITERATIONS_PER_MINUTE = 165
-        self.MAX_WORKERS = 65
+        self.MAX_WORKERS = 50
         self.do_something_after = 0
         self.train_data = []
         self.use_model = use_model
+        self.title = title
 
         # { UNIT_TAG : LOCATION }
         self.scouts_and_spots = {}
 
+        self.choices = {
+            0: self.build_scout,
+            1: self.build_zealot,
+            2: self.build_gateway,
+            3: self.build_voidray,
+            4: self.build_stalker,
+            5: self.build_worker,
+            6: self.build_assimilator,
+            7: self.build_stargate,
+            8: self.build_pylon,
+            9: self.defend_nexus,
+            10: self.attack_known_enemy_unit,
+            11: self.attack_known_enemy_structure,
+            12: self.expand,
+            13: self.do_nothing,
+        }
+
         if self.use_model:
-            print("using model")
+            print("USING MODEL!")
             self.model = keras.models.load_model("models/BasicCNN-30-epochs-0.0001-LR-4.2")
 
     def on_end(self, game_result):
         print('--- on_end called ---')
-        print(game_result)
+        print(game_result, self.use_model)
 
-        if game_result == Result.Victory:
-            np.save("train_data_new/{}.npy".format(str(int(time.time()))), np.array(self.train_data))
+
+        with open("gameout-random-vs-medium.txt","a") as f:
+            if self.use_model:
+                f.write("Model {} - {}\n".format(game_result, int(time.time())))
+            else:
+                f.write("Random {} - {}\n".format(game_result, int(time.time())))
 
     # Execute at every step
     async def on_step(self, iteration):
         # self.iteration = iteration
         self.time = (self.status.game_loop / 22.4) / 60
-        await self.build_scout()
-        await self.scout()
-
         # Initially are 12 workers
         await self.distribute_workers()
-        # Resources
-        await self.build_workers()
-        await self.build_pylons()
-        await self.build_assimilators()
-        await self.expand()
-        # Offensive force
-        await self.offensive_force_buildings()
-        await self.build_offensive_force()
-        await self.attack()
+        # Scouts
+        await self.scout()
         # Deep learning
         await self.intel()
 
-    async def build_scout(self):
-        if len(self.units(OBSERVER)) < math.floor(self.time / 3):
-            for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
-                print(len(self.units(OBSERVER)), self.time / 3)
-                if self.can_afford(OBSERVER) and self.supply_left > 0:
-                    await self.do(rf.train(OBSERVER))
+        await self.do_something()
 
     def random_location_variance(self, enemy_start_location):
         x = enemy_start_location[0]
@@ -165,7 +172,7 @@ class ProtossBot(sc2.BotAI):
                 pos = unit.position
                 cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1], -1)
 
-        main_base_names = ['nexus', 'commandcenter', 'hatchery']
+        main_base_names = ['nexus', 'commandcenter', 'orbitalcommand', 'planetaryfortress', 'hatchery']
         for enemy_building in self.known_enemy_structures:
             pos = enemy_building.position
             if enemy_building.name.lower() not in main_base_names:
@@ -222,152 +229,144 @@ class ProtossBot(sc2.BotAI):
         self.flipped = cv2.flip(game_data, 0)
 
         if not HEADLESS:
-            resize = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
-            cv2.imshow('Intel', resize)
+            resized = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
+            cv2.imshow(str(self.title), resized)
             cv2.waitKey(1)
-
-    # Build workers (PROBE sc2.constants)
-    async def build_workers(self):
-        if len(self.units(NEXUS)) * 16 > len(self.units(PROBE)):
-            if len(self.units(PROBE)) < self.MAX_WORKERS:
-                # Nexus built and without production
-                for nexus in self.units(NEXUS).ready.noqueue:
-                    # If can afford a PROBE
-                    if self.can_afford(PROBE):
-                        # Train the probe
-                        await self.do(nexus.train(PROBE))
-
-    # Build pylons (PYLON sc2.constants)
-    async def build_pylons(self):
-        # Supply capacity left provided by bases and pylon building
-        if self.supply_left < 5 and not self.already_pending(PYLON):
-            # Get nexus
-            nexuses = self.units(NEXUS).ready
-            # Check nexus exists
-            if nexuses.exists:
-                # If can afford a PYLON
-                if self.can_afford(PYLON):
-                    # Build a PYLON near the first nexus
-                    await self.build(PYLON, near=nexuses.first)
-
-    # Build assimilators (ASSIMILATOR sc2.constants)
-    async def build_assimilators(self):
-        # For each nexus ready
-        for nexus in self.units(NEXUS).ready:
-            # Geysers near the nexus
-            vaspenes = self.state.vespene_geyser.closer_than(10.0, nexus)
-            for vaspene in vaspenes:
-                # If still can not build the assimilator
-                if not self.can_afford(ASSIMILATOR):
-                    break
-                # Get workers
-                worker = self.select_build_worker(vaspene.position)
-                # If there are no workers
-                if worker is None:
-                    break
-                # If there are no assimilators near the geysers
-                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
-                    # Build assimilator
-                    await self.do(worker.build(ASSIMILATOR, vaspene))
 
     # Expand the empire
     async def expand(self):
-        # Max 3 nexus
-        if self.units(NEXUS).amount < (self.time / 2) and self.can_afford(NEXUS):
-            await self.expand_now()
+        try:
+            if self.can_afford(NEXUS):
+                await self.expand_now()
+        except Exception as e:
+            print(str(e))
+            
+    async def build_scout(self):
+        if len(self.units(OBSERVER)) < math.floor(self.time / 3):
+            for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
+                print(len(self.units(OBSERVER)), self.time / 3)
+                if self.can_afford(OBSERVER) and self.supply_left > 0:
+                    await self.do(rf.train(OBSERVER))
 
-    # Build offensive force buildings (GATEWAY, CYBERNETICSCORE, STARGATE sc2.constants)
-    async def offensive_force_buildings(self):
-        if self.units(PYLON).ready.exists:
-            # Get a random pylon
-            pylon = self.units(PYLON).ready.random
-            # Build a Cybernetics Core
-            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
+    async def build_scout(self):
+        for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
+            print(len(self.units(OBSERVER)), self.time/3)
+            if self.can_afford(OBSERVER) and self.supply_left > 0:
+                await self.do(rf.train(OBSERVER))
+                break
+
+    async def build_zealot(self):
+        gateways = self.units(GATEWAY).ready
+        if gateways.exists:
+            if self.can_afford(ZEALOT):
+                await self.do(random.choice(gateways).train(ZEALOT))
+
+    async def build_gateway(self):
+        pylon = self.units(PYLON).ready.random
+        if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
+            await self.build(GATEWAY, near=pylon)
+
+    async def build_voidray(self):
+        stargates = self.units(STARGATE).ready
+        if stargates.exists:
+            if self.can_afford(VOIDRAY):
+                await self.do(random.choice(stargates).train(VOIDRAY))
+
+    async def build_stalker(self):
+        pylon = self.units(PYLON).ready.random
+        gateways = self.units(GATEWAY).ready
+        cybernetics_cores = self.units(CYBERNETICSCORE).ready
+
+        if gateways.exists and cybernetics_cores.exists:
+            if self.can_afford(STALKER):
+                await self.do(random.choice(gateways).train(STALKER))
+
+        if not cybernetics_cores.exists:
+            if self.units(GATEWAY).ready.exists:
                 if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
                     await self.build(CYBERNETICSCORE, near=pylon)
-            # Build a Gateway
-            elif len(self.units(GATEWAY)) < 1:
-                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
-                    await self.build(GATEWAY, near=pylon)
-            # Build a Robotics Facility
-            if self.units(CYBERNETICSCORE).ready.exists:
-                if len(self.units(ROBOTICSFACILITY)) < 1:
-                    if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
-                        await self.build(ROBOTICSFACILITY, near=pylon)
-            # Build a Stargate
-            if self.units(CYBERNETICSCORE).ready.exists:
-                if len(self.units(STARGATE)) < self.time:
-                    if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
-                        await self.build(STARGATE, near=pylon)
 
-    # Build offensive army (VOIDRAY sc2.constants)
-    async def build_offensive_force(self):
-        # Stargate
-        for stargate in self.units(STARGATE).ready.noqueue:
-            # Train voidray
-            if self.can_afford(VOIDRAY) and self.supply_left > 0:
-                await self.do(stargate.train(VOIDRAY))
+    async def build_worker(self):
+        nexuses = self.units(NEXUS).ready
+        if nexuses.exists:
+            if self.can_afford(PROBE):
+                await self.do(random.choice(nexuses).train(PROBE))
 
-    # Find location of the enemy (army, structure, etc)
-    def find_target(self, state):
-        # Army or common unit
+    async def build_assimilator(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
+
+    async def build_stargate(self):
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
+                    await self.build(STARGATE, near=pylon)
+
+    async def build_pylon(self):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=self.units(NEXUS).first.position.towards(self.game_info.map_center, 5))
+
+    async def do_nothing(self):
+        wait = random.randrange(7, 100)/100
+        self.do_something_after = self.time + wait
+
+    async def defend_nexus(self):
         if len(self.known_enemy_units) > 0:
-            return random.choice(self.known_enemy_units)
-        # Structure
-        elif len(self.known_enemy_structures) > 0:
-            return random.choice(self.known_enemy_structures)
-        # First location known
-        else:
-            return self.enemy_start_locations[0]
+            target = self.known_enemy_units.closest_to(random.choice(self.units(NEXUS)))
+            for u in self.units(VOIDRAY).idle:
+                await self.do(u.attack(target))
+            for u in self.units(STALKER).idle:
+                await self.do(u.attack(target))
+            for u in self.units(ZEALOT).idle:
+                await self.do(u.attack(target))
 
-    # Attack the enemy
-    async def attack(self):
-        if len(self.units(VOIDRAY).idle) > 0:
-            target = False
-            if self.time > self.do_something_after:
+    async def attack_known_enemy_structure(self):
+        if len(self.known_enemy_structures) > 0:
+            target = random.choice(self.known_enemy_structures)
+            for u in self.units(VOIDRAY).idle:
+                await self.do(u.attack(target))
+            for u in self.units(STALKER).idle:
+                await self.do(u.attack(target))
+            for u in self.units(ZEALOT).idle:
+                await self.do(u.attack(target))
 
-                if self.use_model:
-                    # Use the model
-                    prediction = self.model.predict([self.flipped.reshape([-1, 176, 200, 3])])
-                    choice = np.argmax(prediction[0])
+    async def attack_known_enemy_unit(self):
+        if len(self.known_enemy_units) > 0:
+            target = self.known_enemy_units.closest_to(random.choice(self.units(NEXUS)))
+            for u in self.units(VOIDRAY).idle:
+                await self.do(u.attack(target))
+            for u in self.units(STALKER).idle:
+                await self.do(u.attack(target))
+            for u in self.units(ZEALOT).idle:
+                await self.do(u.attack(target))
 
-                    choice_dict = {
-                        0: "No Attack!",
-                        1: "Attack close to our nexus!",
-                        2: "Attack Enemy Structure!",
-                        3: "Attack Enemy Start!"
-                    }
+    async def do_something(self):
+        if self.time > self.do_something_after:
+            if self.use_model:
+                prediction = self.model.predict([self.flipped.reshape([-1, 176, 200, 3])])
+                choice = np.argmax(prediction[0])
+            else:
+                choice = random.randrange(0, 14)
 
-                    print("Choice #{}:{}".format(choice, choice_dict[choice]))
+            try:
+                await self.choices[choice]()
+            except Exception as e:
+                print(str(e))
 
-                else:
-                    choice = random.randrange(0, 4)
-
-                if choice == 0:
-                    # no attack
-                    wait = random.randrange(7, 100) / 100
-                    self.do_something_after = self.time + wait
-
-                elif choice == 1:
-                    # attack_unit_closest_nexus
-                    if len(self.known_enemy_units) > 0:
-                        target = self.known_enemy_units.closest_to(random.choice(self.units(NEXUS)))
-
-                elif choice == 2:
-                    # attack enemy structures
-                    if len(self.known_enemy_structures) > 0:
-                        target = random.choice(self.known_enemy_structures)
-
-                elif choice == 3:
-                    # attack_enemy_start
-                    target = self.enemy_start_locations[0]
-
-                if target:
-                    for vr in self.units(VOIDRAY).idle:
-                        await self.do(vr.attack(target))
-                y = np.zeros(4)
-                y[choice] = 1
-                self.train_data.append([y, self.flipped])
+            y = np.zeros(14)
+            y[choice] = 1
+            self.train_data.append([y, self.flipped])
 
 
 # Run the game
