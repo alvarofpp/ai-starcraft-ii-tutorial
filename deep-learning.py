45d3bad15920f81228dcb44cbb0ff6521f20b8e0
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-09-17 01:00:11 -0300

    Visualization Changes

--- deep-learning.py
+++ deep-learning.py
@@ -154,82 +154,47 @@ class ProtossBot(sc2.BotAI):
     async def intel(self):
         game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
 
-        # UNIT: [SIZE, (BGR Color)]
-        draw_dict = {
-            NEXUS: [15, (0, 255, 0)],
-            PYLON: [3, (20, 235, 0)],
-            PROBE: [1, (55, 200, 0)],
-            ASSIMILATOR: [2, (35, 200, 0)],
-            GATEWAY: [3, (200, 100, 0)],
-            CYBERNETICSCORE: [3, (150, 150, 0)],
-            STARGATE: [5, (255, 0, 0)],
-            VOIDRAY: [3, (255, 100, 0)],
-            ROBOTICSFACILITY: [5, (215, 155, 0)],
-        }
+        for unit in self.units().ready:
+            pos = unit.position
+            cv2.circle(game_data, (int(pos[0]), int(pos[1])), int(unit.radius*8), (255, 255, 255), math.ceil(int(unit.radius*0.5)))
+        
+        for unit in self.known_enemy_units:
+            pos = unit.position
+            cv2.circle(game_data, (int(pos[0]), int(pos[1])), int(unit.radius*8), (125, 125, 125), math.ceil(int(unit.radius*0.5)))
 
-        for unit_type in draw_dict:
-            for unit in self.units(unit_type).ready:
-                pos = unit.position
-                cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1], -1)
+        try:
+            line_max = 50
+            mineral_ratio = self.minerals / 1500
+            if mineral_ratio > 1.0:
+                mineral_ratio = 1.0
 
-        main_base_names = ['nexus', 'commandcenter', 'orbitalcommand', 'planetaryfortress', 'hatchery']
-        for enemy_building in self.known_enemy_structures:
-            pos = enemy_building.position
-            if enemy_building.name.lower() not in main_base_names:
-                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+            vespene_ratio = self.vespene / 1500
+            if vespene_ratio > 1.0:
+                vespene_ratio = 1.0
 
-        for enemy_building in self.known_enemy_structures:
-            pos = enemy_building.position
-            if enemy_building.name.lower() in main_base_names:
-                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 12, (0, 0, 255), -1)
+            population_ratio = self.supply_left / self.supply_cap
+            if population_ratio > 1.0:
+                population_ratio = 1.0
 
-        for enemy_unit in self.known_enemy_units:
-            if not enemy_unit.is_structure:
-                worker_names = ['probe', 'scv', 'drone']
+            plausible_supply = self.supply_cap / 200.0
 
-                pos = enemy_unit.position
-                if enemy_unit.name.lower() in worker_names:
-                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
-                else:
-                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 155), -1)
+            worker_weight = len(self.units(PROBE)) / (self.supply_cap-self.supply_left)
+            if worker_weight > 1.0:
+                worker_weight = 1.0
 
-        for observer in self.units(OBSERVER).ready:
-            pos = observer.position
-            cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (255, 255, 255), -1)
+            cv2.line(game_data, (0, 19), (int(line_max*worker_weight), 19), (250, 250, 200), 3)  # worker/supply ratio
+            cv2.line(game_data, (0, 15), (int(line_max*plausible_supply), 15), (220, 200, 200), 3)  # plausible supply (supply/200.0)
+            cv2.line(game_data, (0, 11), (int(line_max*population_ratio), 11), (150, 150, 150), 3)  # population ratio (supply_left/supply)
+            cv2.line(game_data, (0, 7), (int(line_max*vespene_ratio), 7), (210, 200, 0), 3)  # gas / 1500
+            cv2.line(game_data, (0, 3), (int(line_max*mineral_ratio), 3), (0, 255, 25), 3)  # minerals minerals/1500
+        except Exception as e:
+            print(str(e))
 
-        # Draw minerals and vespene
-        line_max = 50
-        mineral_ratio = self.minerals / 1500
-        if mineral_ratio > 1.0:
-            mineral_ratio = 1.0
-
-        vespene_ratio = self.vespene / 1500
-        if vespene_ratio > 1.0:
-            vespene_ratio = 1.0
-
-        population_ratio = self.supply_left / self.supply_cap
-        if population_ratio > 1.0:
-            population_ratio = 1.0
-
-        plausible_supply = self.supply_cap / 200.0
-
-        military_weight = len(self.units(VOIDRAY)) / (self.supply_cap - self.supply_left)
-        if military_weight > 1.0:
-            military_weight = 1.0
-
-        cv2.line(game_data, (0, 19), (int(line_max * military_weight), 19), (250, 250, 200), 3)  # worker/supply ratio
-        cv2.line(game_data, (0, 15), (int(line_max * plausible_supply), 15), (220, 200, 200),
-                 3)  # plausible supply (supply/200.0)
-        cv2.line(game_data, (0, 11), (int(line_max * population_ratio), 11), (150, 150, 150),
-                 3)  # population ratio (supply_left/supply)
-        cv2.line(game_data, (0, 7), (int(line_max * vespene_ratio), 7), (210, 200, 0), 3)  # gas / 1500
-        cv2.line(game_data, (0, 3), (int(line_max * mineral_ratio), 3), (0, 255, 25), 3)  # minerals minerals/1500
-
-        # Flip horizontally to make our final fix in visual representation
-        self.flipped = cv2.flip(game_data, 0)
-
+        # flip horizontally to make our final fix in visual representation:
+        grayed = cv2.cvtColor(game_data, cv2.COLOR_BGR2GRAY)
+        self.flipped = cv2.flip(grayed, 0)
+        resized = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
         if not HEADLESS:
-            resized = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
             cv2.imshow(str(self.title), resized)
             cv2.waitKey(1)
 
@@ -240,20 +205,18 @@ class ProtossBot(sc2.BotAI):
                 await self.expand_now()
         except Exception as e:
             print(str(e))
-            
-    async def build_scout(self):
-        if len(self.units(OBSERVER)) < math.floor(self.time / 3):
-            for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
-                print(len(self.units(OBSERVER)), self.time / 3)
-                if self.can_afford(OBSERVER) and self.supply_left > 0:
-                    await self.do(rf.train(OBSERVER))
 
     async def build_scout(self):
         for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
-            print(len(self.units(OBSERVER)), self.time/3)
+            print(len(self.units(OBSERVER)), self.time / 3)
             if self.can_afford(OBSERVER) and self.supply_left > 0:
                 await self.do(rf.train(OBSERVER))
                 break
+        if len(self.units(ROBOTICSFACILITY)) == 0:
+            pylon = self.units(PYLON).ready.noqueue.random
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
+                    await self.build(ROBOTICSFACILITY, near=pylon)
 
     async def build_zealot(self):
         gateways = self.units(GATEWAY).ready
@@ -357,8 +320,18 @@ class ProtossBot(sc2.BotAI):
                 prediction = self.model.predict([self.flipped.reshape([-1, 176, 200, 3])])
                 choice = np.argmax(prediction[0])
             else:
-                choice = random.randrange(0, 14)
+                worker_weight = 8
+                zealot_weight = 3
+                voidray_weight = 20
+                stalker_weight = 8
+                pylon_weight = 5
+                stargate_weight = 5
+                gateway_weight = 3
 
+                choice_weights = 1 * [0] + zealot_weight * [1] + gateway_weight * [2] + voidray_weight * [
+                    3] + stalker_weight * [4] + worker_weight * [5] + 1 * [6] + stargate_weight * [7] + pylon_weight * [
+                                     8] + 1 * [9] + 1 * [10] + 1 * [11] + 1 * [12] + 1 * [13]
+                choice = random.choice(choice_weights)
             try:
                 await self.choices[choice]()
             except Exception as e:
